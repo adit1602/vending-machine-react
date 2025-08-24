7c8b5eb46bdf47875964177a2365ea4dd595f098
Indra Adityawarman <indra2100018060@webmail.uad.ac.id>
2025-08-24 14:08:42 +0700

    feat: implement MainLayout component and update App to use it

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { HistoryPage } from "./pages/HistoryPage";
 import { AdminPage } from "./pages/AdminPage";
-import { Layout } from "@/components/Layout";
-import { ErrorBoundary } from "@/components/ErrorBoundary";
+import { MainLayout, ErrorBoundary } from "@/components";
 // ... other imports
 
 function App() {
   return (
     <ErrorBoundary>
       <BrowserRouter>
-        <Layout>
+        <MainLayout>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/history" element={<HistoryPage />} />
             <Route path="/admin" element={<AdminPage />} />
           </Routes>
-        </Layout>
+        </MainLayout>
       </BrowserRouter>
     </ErrorBoundary>
   );
