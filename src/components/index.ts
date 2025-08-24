a5f78d4c3131863d15881f3abb45f39f759a97c0
Indra Adityawarman <indra2100018060@webmail.uad.ac.id>
2025-08-24 14:15:04 +0700

    style: standardize quotation marks in index.ts component exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,11 @@
 // Export all components from this barrel file
-export { BalancePanel } from './BalancePanel';
-export { ErrorBoundary } from './ErrorBoundary';
-export { MainLayout } from './MainLayout';
-export { LoadingSpinner } from './LoadingSpinner';
-export { ProductCard } from './ProductCard';
-export { ProductForm } from './ProductForm';
-export { ProductManagementTable } from './ProductManagementTable';
-export { TransactionNotification } from './TransactionNotification';
-export { TransactionSummary } from './TransactionSummary';
-export { TransactionTable } from './TransactionTable';
+export { BalancePanel } from "./BalancePanel";
+export { ErrorBoundary } from "./ErrorBoundary";
+export { MainLayout } from "./MainLayout";
+export { LoadingSpinner } from "./LoadingSpinner";
+export { ProductCard } from "./ProductCard";
+export { ProductForm } from "./ProductForm";
+export { ProductManagementTable } from "./ProductManagementTable";
+export { TransactionNotification } from "./TransactionNotification";
+export { TransactionSummary } from "./TransactionSummary";
+export { TransactionTable } from "./TransactionTable";
